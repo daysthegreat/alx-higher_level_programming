22af8d9c2b1a9005fbd97c5f5b01c7964722e5a3
daysthegreat <davisagbevegh@gmail.com>
2023-06-26 12:24:47 +0000

    a function that raises a type exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,4 +1,5 @@
-se_exception():
+#!/usr/bin/python3
+def raise_exception():
     x = "Hello, World!"
     if not isinstance(x, int):
         raise TypeError("Expected an integer, but received a string.")
