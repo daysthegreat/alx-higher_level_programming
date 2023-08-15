166fd88370ca6936f5e3f8723262dcb939d2306a
daysthegreat <davisagbevegh@gmail.com>
2023-08-15 12:59:31 +0000

    SQL - Introduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database 'hbtn_0c_0' in MySQL server
+-- If database already exists, the script should not fail
+-- Not allowed to use SELECT or SHOW
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
